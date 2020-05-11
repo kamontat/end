0151733e6520ba2fb782bdcf43f1a3cab727553b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-05-11 14:56:18 +0700

    perf(config): change some default prettier config

--- packages/stacks/config-prettier/src/index.ts
+++ packages/stacks/config-prettier/src/index.ts
@@ -2,19 +2,27 @@ import { ConfigFunction } from "@nmsys/stack-config";
 
 interface PrettierConfig {
   semi: boolean;
-  trailingComma: "all";
+  trailingComma: "all" | "es5";
   singleQuote: boolean;
   printWidth: number;
   tabWidth: number;
+  useTabs: boolean;
+  bracketSpacing: boolean;
+  arrowParens: "avoid" | "always";
+  endOfLine: "lf" | "crlf" | "cr" | "auto";
 }
 
-const prettier: ConfigFunction<void, PrettierConfig> = (_root) => {
+const prettier: ConfigFunction<void, PrettierConfig> = _root => {
   return {
     semi: true,
-    trailingComma: "all",
+    trailingComma: "es5",
     singleQuote: false,
     printWidth: 120,
     tabWidth: 2,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: "avoid",
+    endOfLine: "lf",
   };
 };
 
