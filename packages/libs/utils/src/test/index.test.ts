b77fd714e556624874f123fca5ad15a10b309171
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-05-17 01:13:28 +0700

    fix(test): add more test

--- packages/libs/utils/src/test/index.test.ts
+++ packages/libs/utils/src/test/index.test.ts
@@ -1,25 +1,39 @@
-
-import { isTesting, isDevelopment, isProduction } from "..";
+import { isTesting, isDevelopment, isProduction, isCI, isStaging } from "..";
 
 describe("Environment helper", () => {
   it("checking testing", () => {
     process.env.NODE_ENV = "test";
 
-    expect(isTesting()).toBeTruthy()
+    expect(isTesting()).toBeTruthy();
     expect(isDevelopment()).toBeFalsy();
   });
 
   it("checking development", () => {
     process.env.NODE_ENV = "d";
 
-    expect(isDevelopment()).toBeTruthy()
+    expect(isDevelopment()).toBeTruthy();
+    expect(isProduction()).toBeFalsy();
+  });
+
+  it("checking development", () => {
+    process.env.NODE_ENV = "stag";
+
+    expect(isStaging()).toBeTruthy();
     expect(isProduction()).toBeFalsy();
   });
 
   it("checking production", () => {
     process.env.NODE_ENV = "production";
 
-    expect(isProduction()).toBeTruthy()
+    expect(isProduction()).toBeTruthy();
     expect(isTesting()).toBeFalsy();
+  });
+
+  it("checking ci", () => {
+    process.env.NODE_ENV = "test";
+    process.env.CI = "true";
+
+    expect(isCI()).toBeTruthy();
+    expect(isTesting()).toBeTruthy();
   });
 });
